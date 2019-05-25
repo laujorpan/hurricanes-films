d87af8477b5e247add667aa42d26cea185e7d5ab
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-05-25 16:05:15 -0300

    El camino es la meta, Que tus pasos sean la semilla de tus hazañas

--- js/renders.js
+++ js/renders.js
@@ -13,3 +13,14 @@ export function resultToCard (result) {
     )
     .join('')
 }
+
+export function resultToDetail (result) {
+  const elem = result[0]
+  return `<div class="nes-container with-title">
+    <p class="title">${elem.title}</p>
+    <div class="grid-2">
+      <img src="https://image.tmdb.org/t/p/w500/${elem.poster_path}" class="detail__img"/>
+      <p style="margin: 0 5rem">${elem.overview}</p>
+    </div>
+  </div>`
+}
--- tests/sort.test.js
+++ tests/sort.test.js
@@ -1,11 +1,21 @@
-import { sortByPolularity } from '../js/sort'
+import { sortByPolularity, sortByVote } from '../js/sort'
 
 describe('test sobre los sorts', () => {
   test('sortByPopularity 1 and 2 give us positive num', () => {
-    expect(sortByPolularity(1, 2)).toBeGreaterThan(0)
+    expect(sortByPolularity({ popularity: 1 }, { popularity: 2 })).toBeGreaterThan(0)
   })
 
   test('sortByPopularity 2 and 1 give us negative num', () => {
-    expect(sortByPolularity(2, 1)).toBeLessThan(0)
+    expect(sortByPolularity({ popularity: 2 }, { popularity: 1 })).toBeLessThan(0)
+  })
+
+  test('sortByPopularity 1 and 2 give us positive num', () => {
+    const result = sortByVote({ popularity: 1, vote_average: 1 }, { popularity: 2, vote_average: 1 })
+    expect(result).toBeGreaterThan(0)
+  })
+
+  test('sortByPopularity 2 and 1 give us negative num', () => {
+    const result = sortByVote({ popularity: 2, vote_average: 1 }, { popularity: 1, vote_average: 1 })
+    expect(result).toBeLessThan(0)
   })
 })
